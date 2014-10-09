50a2c4fd49c957af49c276e0d8c005b5a9b5ebcc
Kees Briggs <kees.briggs@gmail.com>
2014-10-09 12:09:09 -0700

    fix primary key

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,5 +1,6 @@
 class Settings < ActiveRecord::Base
-  self.primary_key = 'key'
+  #self.primary_key = 'key' # causes postgres error on heroku
+  self.primary_key = 'id'
   
   attr_accessible :key, :value
 end
