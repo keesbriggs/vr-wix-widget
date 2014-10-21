f22d0bdf8e49b6888cdfecaf4b2778a243c28aac
Kees Briggs <kees.briggs@gmail.com>
2014-10-20 21:13:39 -0700

    add ajax for oauth

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -13,7 +13,9 @@ class AppController < ActionController::Base
     if params[:code]
       # we need to return to this page as other pages are unauthorized.
       @auth_code = params[:code]
+      puts "KEES: @auth_code is #{@auth_code}"
       @post = "https://vrapi.verticalresponse.com/api/v1/oauth/access_token?client_id=abcdh2wvxxdrw5zanb6wryhc&client_secret=abbcRjmGDCYqRGuyWAs5yJ4C&redirect_uri=https://vr-wix-widget.herokuapp.com/settings?compId=" + @comp_id + "&code=#{@auth_code}"
+      puts "KEES: redirecting to #{@post}"
       redirect_to post_url(@post), status: 302
     end
     value = Settings.find_or_create_by_key(@key).value || '{}'
