7b70b4b5cc157a061517158d75933bcaf8ee0b54
Kees Briggs <kees.briggs@gmail.com>
2014-10-21 15:23:27 -0700

    add ajax for oauth

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -6,8 +6,11 @@ class OauthController < ActionController::Base
       puts "KEES: inside #authenticate - params are #{params.inspect}"
       # we need to return to this page as other pages are unauthorized.
       @auth_code = params[:code]
+      instance = params[:instance].split('.')
+      client_id = @instance.first
+      client_secret = @instance.last
       puts "KEES: @auth_code is #{@auth_code}"
-      @post = "https://vrapi.verticalresponse.com/api/v1/oauth/access_token?client_id=abcdh2wvxxdrw5zanb6wryhc&client_secret=abbcRjmGDCYqRGuyWAs5yJ4C&redirect_uri=https://vr-wix-widget.herokuapp.com/settings?&code=#{@auth_code}"
+      @post = "https://vrapi.verticalresponse.com/api/v1/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&redirect_uri=https://vr-wix-widget.herokuapp.com/settings?&code=#{@auth_code}"
       puts "KEES: redirecting to #{@post}"
       redirect_to @post, status: 302
     end
