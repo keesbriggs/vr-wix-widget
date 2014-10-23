6221582fe499b37cc999ac794428326325327b43
Kees Briggs <kees.briggs@gmail.com>
2014-10-23 15:31:17 -0700

    new secret

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -8,7 +8,7 @@ def savetoken
       client_id = "bruvqhpp3rsp7fwr9vysc8yz"
       client_secret = "T4skBaDmAfkJkZjFg9jbfYHR" # IF THIS IS THE WRONG SECRET THEN HOW DO I GET THE RIGHT ONE???
       @auth_code = params[:code]
-      @post = "https://vrapi.verticalresponse.com/api/v1/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&redirect_uri=https://vr-wix-widget.herokuapp.com/savetoken"
+      @post = "https://vrapi.verticalresponse.com/api/v1/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{@auth_code}&redirect_uri=https://vr-wix-widget.herokuapp.com/savetoken"
       puts "KEES: redirecting to #{@post}"
       redirect_to @post, status: 302
     end
