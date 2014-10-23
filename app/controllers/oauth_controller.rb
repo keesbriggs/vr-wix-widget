40abaf611416e26bbb29f2d8f8d62b743cdf2654
Kees Briggs <kees.briggs@gmail.com>
2014-10-22 21:12:13 -0700

    add ajax for oauth

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -4,7 +4,9 @@ def savetoken
     if params[:code]
       puts "KEES: inside #savetoken - params are #{params.inspect}"
       # we need to return to this page as other pages are unauthorized.
-      client_id = "139aadd7-8703-adde-c4f9-afbb8c7bd505"
+      @settings = Settings.last
+      key = @settings.key
+      client_id = key.split(":").first
       client_secret = "c82ef042-6c35-4ed6-a438-84cdc2a7c119"
       @auth_code = params[:code]
       @post = "https://vrapi.verticalresponse.com/api/v1/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&redirect_uri=https://vr-wix-widget.herokuapp.com/savetoken?&code=#{@auth_code}"
