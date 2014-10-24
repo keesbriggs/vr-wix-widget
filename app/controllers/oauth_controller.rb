185b17fca10b471d5048a4de7f36981d281b97ba
Kees Briggs <kees.briggs@gmail.com>
2014-10-24 15:29:17 -0700

    new secret

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -2,11 +2,10 @@ class OauthController < ActionController::Base
 
 def savetoken
     if params[:code]
-      puts "KEES: inside #savetoken - params are #{params.inspect}"
       # we need to return to this page as other pages are unauthorized.
 
       client_id = "bruvqhpp3rsp7fwr9vysc8yz"
-      client_secret = "T4skBaDmAfkJkZjFg9jbfYHR" # IF THIS IS THE WRONG SECRET THEN HOW DO I GET THE RIGHT ONE???
+      client_secret = "T4skBaDmAfkJkZjFg9jbfYHR" 
       @auth_code = params[:code]
       puts "KEES: @auth_code is #{@auth_code.inspect}"
       @post = "https://vrapi.verticalresponse.com/api/v1/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{@auth_code}&redirect_uri=https://vr-wix-widget.herokuapp.com/savetoken"
