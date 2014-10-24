3ab03ebc45cf53fb1606c83cee1d2645ce84b510
Kees Briggs <kees.briggs@gmail.com>
2014-10-24 15:53:58 -0700

    new secret

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -7,12 +7,12 @@ class OauthController < ActionController::Base
       client_id = "bruvqhpp3rsp7fwr9vysc8yz"
       client_secret = "T4skBaDmAfkJkZjFg9jbfYHR" 
       @auth_code = params[:code]
-      redirect_to "https://vrapi.verticalresponse.com/api/v1/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{@auth_code}&redirect_uri=https://vr-wix-widget.herokuapp.com/savetoken", :status => 302
+      redirect_to "https://vrapi.verticalresponse.com/api/v1/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{@auth_code}&redirect_uri=https://vr-wix-widget.herokuapp.com/saveaccesstoken", :status => 302
     end
   end
 
   def saveaccesstoken
-    debugger
+    
     puts "KEES: params  is #{params.inspect}"
   end  
 end
