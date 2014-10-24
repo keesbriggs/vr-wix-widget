a285e1391c79ff6b7760b9dba0c44259bb05843e
Kees Briggs <kees.briggs@gmail.com>
2014-10-24 15:56:22 -0700

    new secret

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,18 +1,22 @@
 class OauthController < ActionController::Base
 
   def savetoken
+    puts "KEES: params  is #{params.inspect}"
     if params[:code]
       # we need to return to this page as other pages are unauthorized.
 
       client_id = "bruvqhpp3rsp7fwr9vysc8yz"
       client_secret = "T4skBaDmAfkJkZjFg9jbfYHR" 
       @auth_code = params[:code]
-      redirect_to "https://vrapi.verticalresponse.com/api/v1/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{@auth_code}&redirect_uri=https://vr-wix-widget.herokuapp.com/saveaccesstoken", :status => 302
+      redirect_to "https://vrapi.verticalresponse.com/api/v1/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{@auth_code}&redirect_uri=https://vr-wix-widget.herokuapp.com/savetoken", :status => 302
+    elsif params[:user_id]
+      # save access token here!
     end
+
   end
 
   def saveaccesstoken
     
-    puts "KEES: params  is #{params.inspect}"
+    
   end  
 end
