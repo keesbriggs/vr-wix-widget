3b1a57eadf718a0335952314dbac75e1646b5d48
Kees Briggs <kees.briggs@gmail.com>
2014-10-25 16:12:42 -0700

    fix user

--- db/migrate/20141025191703_create_user_table.rb
+++ db/migrate/20141025191703_create_user_table.rb
@@ -1,7 +1,7 @@
 class CreateUserTable < ActiveRecord::Migration
   def change
   	create_table :users do |t|
-      t.integer :vr_user_id
+      t.integer :vr_user_id, :limit => 8
       t.string  :access_token
  
       t.timestamps
