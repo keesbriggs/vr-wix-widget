7d45f90cee2c535ab33b08c302873c6510190ebf
Kees Briggs <kees.briggs@gmail.com>
2014-10-25 15:46:15 -0700

    fix user

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -25,10 +25,9 @@ class OauthController < ActionController::Base
 
       response_json = JSON.parse(response.body)
       puts "KEES: response_json is #{response_json.inspect}"
+      puts "KEES: response_json.class is #{response_json.class.inspect}"
       puts "KEES: response_json[:user_id] is #{response_json[:user_id].inspect}"
       puts "KEES: response_json[:access_token] is #{response_json[:access_token].inspect}"
-      puts "KEES: response_json.user_id is #{response_json.user_id.inspect}"
-      puts "KEES: response_json.access_token is #{response_json.access_token.inspect}"
       user = User.create({ vr_user_id: response_json[:user_id], access_token: response_json[:access_token] })
     end
   end
