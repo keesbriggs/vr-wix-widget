814bdfe7b9a68bcac91795be3ba19e900d2622f2
Kees Briggs <kees.briggs@gmail.com>
2014-10-31 13:17:07 -0700

    fix lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,8 @@
-class List < Struct.new(
-  :name, :is_public, :public_name)
+class List < Struct.new(:name, :is_public, :public_name)
 
-  def initialize(attributes = {})
-    super(*attributes.values_at(*self.class.members))
-  end
+  attr_accessor :name, :is_public, :public_name
+
+  attr_accessible :name, :is_public, :public_name
+
+  validates :name, :is_public, :public_name, presence: true
 end
