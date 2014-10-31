f44e878f474cfc61c35f6f0cf625ff9010bf4962
Kees Briggs <kees.briggs@gmail.com>
2014-10-31 16:19:19 -0700

    fix lists

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -55,7 +55,7 @@ class AppController < ActionController::Base
       @lists = @lists || []
       response_json[:items].each do |list|
         puts "KEES: inside SETTINGS loop - list is #{list.inspect}"
-        @lists << List.new(list[:attributes[:name], list[:attributes[:is_public], list[:attributes[:public_name])
+        @lists << List.new(list[:attributes][:name], list[:attributes][:is_public], list[:attributes][:public_name])
       end
       puts "KEES: inside SETTINGS - @lists is #{@lists.inspect}"
     end      
