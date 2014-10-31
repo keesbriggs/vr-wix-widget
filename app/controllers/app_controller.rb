d46dfb5dccc05f6038974588d261859aee9a17ab
Kees Briggs <kees.briggs@gmail.com>
2014-10-31 13:03:49 -0700

    fix lists

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -45,9 +45,11 @@ class AppController < ActionController::Base
       response_json = JSON.parse(response.body)
       puts "KEES: inside SETTINGS - response_json is #{response_json}"
       @lists = @lists || []
-      response_json["items"].each do |list|
-        @lists << List.new list.attributes
-      end if @lists == []
+      if @lists == []
+        response_json["items"].each do |list|
+          @lists << List.new list.attributes
+        end 
+      end
       puts "KEES: inside SETTINGS - @lists is #{@lists.inspect}"
     end      
 
