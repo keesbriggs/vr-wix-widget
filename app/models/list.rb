12c71576d844a31078fdd9c7427156555458c677
Kees Briggs <kees.briggs@gmail.com>
2014-10-31 13:40:12 -0700

    fix lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,3 @@
 class List < Struct.new(
 	:name, :is_public, :public_name)
-
-  #attr_accessor :name, :is_public, :public_name
-
-  #attr_accessible :name, :is_public, :public_name
-
-  #validates :name, :is_public, :public_name, presence: true
 end
